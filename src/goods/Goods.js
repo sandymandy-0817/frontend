1560a4bbad0eb7d82341d17b4f867dfa89d9c314
sandymandy-0817 <ksk3570@gmail.com>
2025-06-05 12:55:00 +0900

    Update Goods.js

--- src/goods/Goods.js
+++ src/goods/Goods.js
@@ -29,7 +29,7 @@ function Goods(props) {
         console.log('삭제할 g_code:', g_code);
         if(window.confirm('정말 삭제하시겠습니까?')) {
             axios
-                .delete(`http://localhost:9070/goods/${g_code}`)
+                .delete(`https://port-0-backend-mbha4admd1f88f85.sel4.cloudtype.app/goods/goods/${g_code}`)
                 .then(() => {
                     alert('삭제되었습니다');
                     loadData();
