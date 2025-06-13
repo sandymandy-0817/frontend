7722474272bf0587957dc0d9f21870d824229e11
sandymandy-0817 <ksk3570@gmail.com>
2025-06-13 12:45:53 +0900

    Update Goods.js

--- src/goods/Goods.js
+++ src/goods/Goods.js
@@ -13,7 +13,7 @@ function Goods(props) {
 
     const loadData = () => {
         axios
-        .get('backend:9070/goods')
+        .get('https://port-0-backend-mbha4admd1f88f85.sel4.cloudtype.app/goods')
         .then(res=>{
             setItem(res.data)
             setGoodsCount(res.data.length);
@@ -29,7 +29,7 @@ function Goods(props) {
         console.log('삭제할 g_code:', g_code);
         if(window.confirm('정말 삭제하시겠습니까?')) {
             axios
-                .delete(`backend:9070/goods/${g_code}`)
+                .delete(`https://port-0-backend-mbha4admd1f88f85.sel4.cloudtype.app/goods/${g_code}`)
                 .then(() => {
                     alert('삭제되었습니다');
                     loadData();
